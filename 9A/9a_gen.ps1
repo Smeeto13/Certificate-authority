fd6b65eb6f1850b357dc06b38f21169cf76cb654
Oliver Smeeton <oliversmeeton2019@gmail.com>
2023-02-06 23:30:31 +0000

    update configs and add 9a config

---
+++ 9A/9a_gen.ps1
@@ -0,0 +1,9 @@
+﻿openssl ecparam -genkey -name secp384r1 -out 9A.key
+
+openssl req -sha256 -new -config 9a.cnf -key 9A.key -nodes -out 9A.csr
+
+openssl x509 -req -in 9A.csr -CA ../CA.crt -CAkey ../CA.key -CAcreateserial -out 9A.crt -days 1825 -extensions v3_req -extfile 9a.cnf
+
+openssl pkcs12 -export -out 9A.pfx -inkey 9A.key -in 9A.crt -certfile ../CA.crt
+
+rm 9A.csr
--- 9A/9a_gen.sh
+++ 9A/9a_gen.sh
@@ -1,11 +1,11 @@
-export name="9C"
-export cfg="9c"
+export name="9A"
+export cfg="9a"
 
 openssl ecparam -genkey -name secp384r1 -out $name.key
 
 openssl req -sha256 -new -config $cfg.cnf -key $name.key -nodes -out $name.csr
 
-openssl x509 -req -in $name.csr -CA ../CA.crt -CAkey ../CA.key -set_serial 2 -out $name.crt -days 1825 -extensions v3_req -extfile $cfg.cnf
+openssl x509 -req -in $name.csr -CA ../CA.crt -CAkey ../CA.key -CAcreateserial -out $name.crt -days 1825 -extensions v3_req -extfile $cfg.cnf
 
 openssl pkcs12 -export -out $name.pfx -inkey $name.key -in $name.crt -certfile ../CA.crt
 
