92d721d4204d36f63aad35df9dc0e784f81a291c
Oliver Smeeton <oliversmeeton2019@gmail.com>
2023-02-07 20:06:40 +0000

    Updates scripts to auto generate serial number

--- 9C/9c_gen.ps1
+++ 9C/9c_gen.ps1
@@ -2,7 +2,7 @@
 
 openssl req -sha256 -new -config 9c.cnf -key 9C.key -nodes -out 9C.csr
 
-openssl x509 -req -in 9C.csr -CA ../CA.crt -CAkey ../CA.key -set_serial 2 -out 9C.crt -days 1825 -extensions v3_req -extfile 9c.cnf
+openssl x509 -req -in 9C.csr -CA ../CA.crt -CAkey ../CA.key -CAcreateserial -out 9C.crt -days 1825 -extensions v3_req -extfile 9c.cnf
 
 openssl pkcs12 -export -out 9C.pfx -inkey 9C.key -in 9C.crt -certfile ../CA.crt
 
--- 9E/9e_gen.sh
+++ 9E/9e_gen.sh
@@ -5,7 +5,7 @@ openssl genrsa -out $name.key 2048
 
 openssl req -sha256 -new -config $cfg.cnf -key $name.key -nodes -out $name.csr
 
-openssl x509 -req -in $name.csr -CA ../CA.crt -CAkey ../CA.key -set_serial 3 -out $name.crt -days 1825 -extensions v3_req -extfile $cfg.cnf
+openssl x509 -req -in $name.csr -CA ../CA.crt -CAkey ../CA.key -CAcreateserial -out $name.crt -days 1825 -extensions v3_req -extfile $cfg.cnf
 
 openssl pkcs12 -export -out $name.pfx -inkey $name.key -in $name.crt -certfile ../CA.crt
 
