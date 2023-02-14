cfd643f8c7fc4dc551fb0a679ad5d9c0c8ccbfc0
Oliver Smeeton <oliversmeeton2019@gmail.com>
2023-02-14 13:06:36 +0000

    Change CRL name and URI

--- CA.py
+++ CA.py
@@ -57,7 +57,7 @@ def main():
     if prompt("Generate CRL"):
         if passwd == "":
             passwd = input("PEM Password: ")
-        os.system(f"openssl ca -config Root-CA.cnf -keyfile CA.key -cert certificates/CA.pem -gencrl -out CA-CRL.pem -passin pass:{passwd}")
+        os.system(f"openssl ca -config Root-CA.cnf -keyfile CA.key -cert certificates/CA.pem -gencrl -out CRL.pem -passin pass:{passwd}")
 
 
 main()
