c626cb35a3812f81271329713ec3c2ff5f299a99
Sophie Smeeton <65131041+sophie-s1@users.noreply.github.com>
2025-02-12 22:56:48 +0000

    switch smart card back to RSA

--- Backend.py
+++ Backend.py
@@ -61,7 +61,7 @@ class CertificateAuthority:
                 f"openssl req -new -x509 -sha512 -days 3650 -config Root-CA.cnf -extensions v3_ca -set_serial 1 -key CA.key -out certificates/CA.pem -passout pass:{self.passwd}")
         elif self.use_sc and not os.path.isfile("CA_SN.conf"):
             serial_number = uuid.uuid4().hex
-            os.system(f"""pkcs11-tool --login --keypairgen --key-type EC:prime256v1 --label "CA" --id {serial_number}""")
+            os.system(f"""pkcs11-tool --login --keypairgen --key-type rsa:2048 --label "CA" --id {serial_number}""")
             os.system(f"openssl req -config Root-CA.cnf -engine pkcs11 -keyform engine -key {serial_number} -new -x509 -days 3650 -sha512 -extensions v3_ca -set_serial 1 -out certificates/CA.pem")
             os.system("pkcs15-init -X certificates/CA.pem")
             with open("CA_SN.conf","w") as ca_sn:
