184639119ca397f28203a75a5acd58582c145dff
Sophie Smeeton <65131041+Smeeto13@users.noreply.github.com>
2025-02-12 00:13:21 +0000

    Add Smart Card support

--- Backend.py
+++ Backend.py
@@ -1,4 +1,4 @@
-""" Copyright 2023, 2024 Sophie Smeeton
+""" Copyright 2023, 2024, 2025 Sophie Smeeton
 This file is part of Certificate-authority-Tool
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -8,7 +8,7 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.  """
 
 import os
-
+import uuid
 
 class CertificateAuthority:
     """Behind the scenes operations"""
@@ -27,6 +27,27 @@ class CertificateAuthority:
             os.system("mkdir -f out")
             os.system("mkdir -f certificates")
 
+    def set_sc(self, use_sc):
+        """Sets if the CA is stored on a Smart Card"""
+        match use_sc:
+            case "PKCS11 CA":
+                self.use_sc = True
+                self.opts = "-engine pkcs11 -keyform engine"
+                if os.path.isfile("CA_SN.conf"):
+                    with open("CA_SN.conf","r") as ca_sn:
+                        self.key_file = ca_sn.read()
+                    print(self.key_file)
+                else:
+                    print("No key ID in CA_SN.conf, Generating new CA")
+                    self.create_ca()
+            case "Software CA":
+                self.use_sc = False
+                self.key_file = "CA.key"
+                self.opts = f"-passin pass:{self.passwd}"
+                if not os.path.isfile("CA.key"):
+                    print("No CA key found, Generating new CA")
+                    self.create_ca()
+
     def set_pass(self, passwd):
         """Set's the password to use for the CA key and PFX exports"""
 
@@ -34,14 +55,21 @@ class CertificateAuthority:
 
     def create_ca(self):
         """Create a self signed CA"""
-        if not os.path.isfile("CA.key"):
+        if not os.path.isfile("CA.key") and not self.use_sc:
             os.system(
                 f"openssl req -new -x509 -sha512 -days 3650 -config Root-CA.cnf -extensions v3_ca -set_serial 1 -keyout CA.key -out certificates/CA.pem -passout pass:{self.passwd}")
-            os.system(
-                "openssl x509 -outform der -in certificates/CA.pem -out out/CA.crt")
-            os.system("echo 02 > serial")
+        elif self.use_sc and not os.path.isfile("CA_SN.conf"):
+            serial_number = uuid.uuid4().hex
+            os.system(f"""pkcs11-tool --login --keypairgen --key-type rsa:2048 --label "CA" --id {serial_number}""")
+            os.system(f"openssl req -config Root-CA.cnf -engine pkcs11 -keyform engine -key {serial_number} -new -x509 -days 3650 -sha512 -extensions v3_ca -set_serial 1 -out certificates/CA.pem")
+            os.system("pkcs15-init -X certificates/CA.pem")
+            with open("CA_SN.conf","w") as ca_sn:
+                ca_sn.write(serial_number)
+            self.key_file = serial_number
         else:
-            print("\"CA.key\" already exists")
+            print("CA key already exists")
+        os.system("openssl x509 -outform der -in certificates/CA.pem -out out/CA.crt")
+        os.system("echo 02 > serial")
 
     def key_gen(self, name, csr, cfg, key_type):
         """Generates key and CSR"""
@@ -71,10 +99,10 @@ class CertificateAuthority:
         os.system(f"mkdir -p out/{name}")
         if cfg == "No CNF (CSR Signing only)":
             os.system(
-                f"openssl ca -config {ca_config} -in csr/{csr} -out out/{name}/{name}.crt -extensions copy -notext -passin pass:{self.passwd}")
+                f"openssl ca -config {ca_config} -keyfile {self.key_file} -in csr/{csr} -out out/{name}/{name}.crt -extensions copy -notext {self.opts}")
         else:
             os.system(
-                f"openssl ca -config {ca_config} -in csr/{csr} -out out/{name}/{name}.crt -extensions v3_req -extfile cnf/{cfg} -notext -passin pass:{self.passwd}")
+                f"openssl ca -config {ca_config} -keyfile {self.key_file} -in csr/{csr} -out out/{name}/{name}.crt -extensions v3_req -extfile cnf/{cfg} -notext {self.opts}")
         os.system(f"rm csr/{csr}")
 
     def pfx_out(self, name):
@@ -86,9 +114,9 @@ class CertificateAuthority:
     def crt_revoke(self, revoke, ca_config="Root-CA.cnf"):
         """Revoke a certificate, specify ca_config to revoke a certificate from a intermediate CA"""
         os.system(
-            f"openssl ca -revoke certificates/{revoke} -config {ca_config} -passin pass:{self.passwd}")
+            f"openssl ca -revoke certificates/{revoke} -config {ca_config} -keyfile {self.key_file} {self.opts}")
 
     def create_crl(self, ca_config="Root-CA.cnf"):
         """generates the crl for a CA"""
         os.system(
-            f"openssl ca -config {ca_config} -gencrl -out out/CRL.pem -passin pass:{self.passwd}")
+            f"openssl ca -config {ca_config} -keyfile {self.key_file} -gencrl -out out/CRL.pem {self.opts}")
--- CA_Menu.py
+++ CA_Menu.py
@@ -1,4 +1,4 @@
-""" Copyright 2023, 2024 Sophie Smeeton
+""" Copyright 2023, 2024, 2025 Sophie Smeeton
 This file is part of Certificate-authority-Tool
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -10,7 +10,6 @@ You should have received a copy of the GNU General Public License along with thi
 import os
 import getpass
 import Backend
-
 
 def selection_menu(options):
     """
@@ -86,6 +85,7 @@ CA = Backend.CertificateAuthority()
 
 def main():
     CA.set_pass(pass_in())
+    CA.set_sc(selection_menu(["PKCS11 CA", "Software CA"]))
     exit_menu = False
 
     # Main Menu
